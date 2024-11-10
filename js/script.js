60c7dd82e5bc31face81db349ed250c351a0ba85
AngusPanero <aguspanero@gmail.com>
2024-11-10 15:21:38 +0100

    Peliculas ya Filtradas + Empezando a Crear Hijos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 import peliculas from './peliculas.js'
 
-console.log(peliculas);
+//Filtrar Peliculas Acción id=28
+const accion = peliculas.filter((element) => element.genre_ids.includes(28));
+console.log("Peliculas de Acción", accion);
+
+//Filtrar Peliculas Thriller id=53
+const thriller = peliculas.filter((element) => element.genre_ids.includes(53));
+console.log("Peliculas de Thriller", thriller);
+
+//Filtrar Peliculas Aventura id=12
+const aventura = peliculas.filter((element) => element.genre_ids.includes(12));
+console.log("Peliculas de Aventura", aventura);  
+
+//Crear Nodo que Sume un <figure> a div "Genero-28"
+const llamarGen28 = document.getElementById("genero-28");
+console.log(llamarGen28);
+
+const crearFig28 = document.createElement("figure");
+llamarGen28.appendChild(crearFig28)
+console.log(llamarGen28);
